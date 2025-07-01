75819293ab522c301d1f73fd888c59eeeabe229e
webDevMatt-dot <mgluis630@gmail.com>
2025-07-01 12:50:52 +0200

    Menu to the right

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Navbar() {
   return (
-    <header className="text-black fixed top-0 left-0 right-0 bg-white border-b border-gray-300 shadow-lg z-50 p-4 flex items-center justify-start">
+    <header className="text-black fixed top-0 left-0 right-0 bg-white border-b border-gray-300 shadow-lg z-50 p-4 flex items-center justify-end">
       <nav className="space-x-6">
         <Link href="/" className="hover:underline">Home</Link>
         <Link href="/#services" className="hover:underline">Services</Link>
