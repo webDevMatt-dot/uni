e41979a2566573d095e72d01f69c9efa95e3660f
webDevMatt-dot <mgluis630@gmail.com>
2025-07-01 14:50:18 +0200

    NavBar padding update

--- .next/server/chunks/ssr/_3a1df5ae._.js
+++ .next/server/chunks/ssr/_3a1df5ae._.js
@@ -25,7 +25,7 @@ var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist
 ;
 function Navbar() {
     return /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("header", {
-        className: "text-black fixed top-0 left-0 right-0 bg-white border-b border-gray-300 shadow-lg z-50 p-4 flex items-center justify-end",
+        className: "text-black fixed top-0 left-0 right-0 bg-white border-b border-gray-300 shadow-lg z-50 h-20 flex items-center justify-end px-6",
         children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("nav", {
             className: "space-x-6",
             children: [
