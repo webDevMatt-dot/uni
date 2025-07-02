811de0b640b29c0bc5c80074004077133fe98e3a
webDevMatt-dot <mgluis630@gmail.com>
2025-07-02 09:54:40 +0200

    add submit button

--- .next/server/chunks/ssr/_75452c72._.js
+++ .next/server/chunks/ssr/_75452c72._.js
@@ -35,7 +35,7 @@ function Navbar() {
                     href: "/",
                     "aria-label": "Go to homepage",
                     children: /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])(__TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$next$2f$image$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["default"], {
-                        src: "/logo.ico",
+                        src: "/globe.svg",
                         alt: "AAT Business Solutions Logo",
                         width: 50,
                         height: 50,
