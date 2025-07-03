fb89e56942ae2d3c127b4784ff296e05616c622d
webDevMatt-dot <mgluis630@gmail.com>
2025-07-03 20:35:34 +0200

    website re-upload

--- .next/static/chunks/app_contact-pages_page_tsx_be47e2ec._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_cc782f87._.js",
-  "static/chunks/_287fe74f._.js",
-  "static/chunks/src_components_ui_team-gallery_45509a21.css"
-],
-    source: "dynamic"
-});
--- .next/static/chunks/app_services_cybersecurity_page_tsx_be47e2ec._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_cc782f87._.js",
-  "static/chunks/_f8d962b2._.js"
-],
-    source: "dynamic"
-});
