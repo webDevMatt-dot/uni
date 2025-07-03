1844c02ecf6c018c835857f717dc87d8bf39be8e
webDevMatt-dot <mgluis630@gmail.com>
2025-07-03 21:12:06 +0200

    website re-upload

--- app/contact-pages/page.tsx
+++ app/contact-pages/page.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import Head from "next/head";
 import { Navbar } from "src/components/ui/Navbar";
-import { Card, CardContent } from "src/components/ui/card";
-import { Footer } from "src/components/ui/Footer";
-import { TeamGallery } from "src/components/ui/TeamGallery";
 
 export default function ContactPage() {
   return (
