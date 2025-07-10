ed7c174d380e8d4798cd250968fd631889d86f2d
webDevMatt-dot <mgluis630@gmail.com>
2025-07-10 17:06:13 +0200

    loader page + logo from netlify

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -6,7 +6,7 @@ export function Loader() {
   return (
     <div className="fixed inset-0 bg-white z-[9999] flex items-center justify-center">
       <Image
-        src="https://github.com/webDevMatt-dot/aat/blob/main/public/logo.png" // ✅ Update this to your actual logo path
+        src="https://aat-website.netlify.app/logo.png" // ✅ Update this to your actual logo path
         alt="AAT Business Solutions Logo"
         width={350}
         height={350}
