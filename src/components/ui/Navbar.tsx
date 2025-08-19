376999857c7e75474a0938eaa9f1289b5df07874
webDevMatt-dot <mgluis630@gmail.com>
2025-08-19 11:27:22 +0200

    UPDATE: edit navBar page for the image (avast.png test)

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -34,7 +34,7 @@ export function Navbar() {
           onClick={() => setMenuOpen(false)}
         >
           <Image
-            src="/logo.ico" // Your Unison logo image
+            src="/avast.png" // Your Unison logo image
             alt="Unison Logo" // More descriptive alt text
             width={scrolled ? 38 : 46} // Image size controlled by scroll
             height={scrolled ? 38 : 46} // Image size controlled by scroll
